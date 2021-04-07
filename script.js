f46a97f4e8bc6a23584baa3a04f5a8860059ecd3
webgtx <dxv1d@tuta.io>
2021-04-07 16:44:00 +0300

    added eventListener + broken displayMessager

---
+++ script.js
@@ -0,0 +1,29 @@
+let message = document.querySelector('.message');
+let btnAdd = document.querySelector('.add');
+let todo = document.querySelector('.todo');
+
+let todoList = [];
+
+btnAdd.addEventListener('click',() => {
+    let newTodo = {
+        info: message.value,
+        checked: false,
+        important: false
+    }
+    todoList.push(newTodo);
+    displayMessage();
+})
+
+function displayMessage() {
+    let dislpayMessage
+    todoList.forEach((item, index) => {
+        dislpayMessage += `
+        <li>
+            <input type="checkbox" id="item_${index}">
+            <label for="item_${index}">${item.info}</label>
+        </li>
+        `;
+        todo.innerHTML = dislpayMessage;
+    })
+}
+
