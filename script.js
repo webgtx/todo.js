80eb31ef23f2ee6c24385e0ef3d2b2f6e41fa34a
webgtx <dxv1d@tuta.io>
2021-06-28 17:36:59 +0300

    added new feature (delete button), rewrited idx

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let message = document.querySelector('.message');
 let btnAdd = document.querySelector('.add');
 let todo = document.querySelector('.todo');
+let btnDelete
 
 let todoList = [];
 
@@ -8,6 +9,7 @@ if (localStorage.getItem('todo')) {
   todoList = JSON.parse(localStorage.getItem('todo'));
   displayMessage();
 }
+
 
 btnAdd.addEventListener('click', () => {
     let newTodo = {
@@ -22,8 +24,10 @@ btnAdd.addEventListener('click', () => {
 
 todo.addEventListener('change', event => {
   let valueLabel = todo.querySelector(`[for='${event.target.getAttribute('id')}']`).innerHTML;
-
   todoList.forEach(item => {
+      if (item == null) {
+    	return
+    }
     if (item.info === valueLabel) {
       item.checked = !item.checked;
       localStorage.setItem('todo', JSON.stringify(todoList));
@@ -32,14 +36,29 @@ todo.addEventListener('change', event => {
 })
 
 function displayMessage() {
-    let logMessage = ""
+    let logMessage = "";
+    if (todoList.length == 0) {
+    	logMessage = '<h1>Nothing there</h1>';
+    	todo.innerHTML = logMessage;
+    }
     todoList.forEach((item, index) => {
       logMessage += `
           <li class="todo__list" id="${index}">
-              <input type="checkbox" id="item_${index}" ${item.checked ? 'checked' : ''}>
+              <input type="checkbox" id="item_${index}" ${!item.checked ? '' : 'checked'}>
               <label for="item_${index}">${item.info}</label>
+              <button id="${index}" class="todo-delete">Delete</button>
           </li>`;
       todo.innerHTML = logMessage;
+      
+      todoList.length > 0 ? btnDelete = document.querySelectorAll('.todo-delete') : null;
+      btnDelete.forEach((item) => {
+      	item.addEventListener('click', () => {
+      		let idx = item.getAttribute('id');
+      		todoList.splice(idx, 1);
+      		displayMessage();
+      		localStorage.setItem('todo', JSON.stringify(todoList));
+      	})
+      })
     })
 }
 
